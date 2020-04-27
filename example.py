9dccf5422e897d329419d54566fb7c06032965b5
niels <51261230+nmakel@users.noreply.github.com>
2020-04-27 15:46:12 +0200

    added json output mode

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
 import solaredge_modbus
 
 
@@ -9,9 +10,12 @@ if __name__ == "__main__":
     argparser.add_argument("host", type=str, help="ModbusTCP address")
     argparser.add_argument("port", type=int, help="ModbusTCP port")
     argparser.add_argument("--unit", type=int, default=1, help="Modbus unit")
+    argparser.add_argument("--json", action="store_true", default=False, help="Output as JSON")
     args = argparser.parse_args()
 
     inverter = solaredge_modbus.Inverter(host=args.host, port=args.port, unit=args.unit)
 
-    print(inverter.read_all())
-    inverter.pprint()
+    if args.json:
+        print(json.dumps(inverter.read_all(), indent=4))
+    else:
+        inverter.pprint()
