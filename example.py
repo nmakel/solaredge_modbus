8c4299284c1ab1b665b20bd11797960d895e5d78
niels <51261230+nmakel@users.noreply.github.com>
2020-04-29 13:29:44 +0200

    added timeout parameter

--- example.py
+++ example.py
@@ -9,11 +9,17 @@ if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument("host", type=str, help="ModbusTCP address")
     argparser.add_argument("port", type=int, help="ModbusTCP port")
+    argparser.add_argument("--timeout", type=int, default=1, help="Connection timeout")
     argparser.add_argument("--unit", type=int, default=1, help="Modbus unit")
     argparser.add_argument("--json", action="store_true", default=False, help="Output as JSON")
     args = argparser.parse_args()
 
-    inverter = solaredge_modbus.Inverter(host=args.host, port=args.port, unit=args.unit)
+    inverter = solaredge_modbus.Inverter(
+        host=args.host,
+        port=args.port,
+        timeout=args.timeout,
+        unit=args.unit
+    )
 
     if args.json:
         print(json.dumps(inverter.read_all(), indent=4))
