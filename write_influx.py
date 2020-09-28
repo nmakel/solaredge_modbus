08f06e47c12a201f360d8e212c58afca65b45962
Leonid Verhovskij <l.verhovskij@gmail.com>
2020-09-28 23:38:08 +0200

    add endless loop

--- write_influx.py
+++ write_influx.py
@@ -5,6 +5,7 @@ import json
 import solaredge_modbus
 from influxdb import InfluxDBClient
 from datetime import datetime
+import time
 
 
 if __name__ == "__main__":
@@ -30,102 +31,105 @@ if __name__ == "__main__":
         unit=args.unit
     )
 
-    values = {}
-    values = inverter.read_all()
-    meters = inverter.meters()
-    batteries = inverter.batteries()
+    while True:
 
-    # prepare data for influxdb
-    json_body = []
-    current_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
-    # inverter, meters, batteries
+        values = {}
+        values = inverter.read_all()
+        meters = inverter.meters()
+        batteries = inverter.batteries()
 
-    inverter_data = {
-        "measurement": "inverter",
-        "tags": {
-            "c_model": values["c_model"],
-            "c_version": values["c_version"],
-            "c_serialnumber": values["c_serialnumber"],
-            "c_deviceaddress": values["c_deviceaddress"],
-            "c_sunspec_did": values["c_sunspec_did"]
-        },
-        "time": current_time,
-        "fields": {}
-    }
-    for key,value in values.items():
-        if (isinstance(value, int) or isinstance(value, float)) and '_scale' not in key:
-            # search scale
-            splitted_key = key.split("_")
-            scale = 0
-            if splitted_key[len(splitted_key)-1] + "_scale" in values:
-                scale = values[splitted_key[len(splitted_key)-1] + "_scale"]
-            elif key + "_scale" in values:
-                scale = values[key + "_scale"]
+        # prepare data for influxdb
+        json_body = []
+        current_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+        # inverter, meters, batteries
 
-            if scale < 0:
-                value = value / 10**abs(scale)
-
-            inverter_data["fields"].update({key: value})
-
-    json_body.append(inverter_data)
-
-    # read meters
-    for meter,params in meters.items():
-        meter_values = params.read_all()
-        meter_data = {
-            "measurement": "meter",
+        inverter_data = {
+            "measurement": "inverter",
             "tags": {
-                "c_model": meter_values["c_model"],
-                "c_option": meter_values["c_option"],
-                "c_version": meter_values["c_version"],
-                "c_serialnumber": meter_values["c_serialnumber"],
-                "c_deviceaddress": meter_values["c_deviceaddress"],
-                "c_sunspec_did": meter_values["c_sunspec_did"]
+                "c_model": values["c_model"],
+                "c_version": values["c_version"],
+                "c_serialnumber": values["c_serialnumber"],
+                "c_deviceaddress": values["c_deviceaddress"],
+                "c_sunspec_did": values["c_sunspec_did"]
             },
             "time": current_time,
             "fields": {}
         }
-
-        for key,value in meter_values.items():
+        for key,value in values.items():
             if (isinstance(value, int) or isinstance(value, float)) and '_scale' not in key:
                 # search scale
                 splitted_key = key.split("_")
                 scale = 0
                 if splitted_key[len(splitted_key)-1] + "_scale" in values:
-                    scale = meter_values[splitted_key[len(splitted_key)-1] + "_scale"]
+                    scale = values[splitted_key[len(splitted_key)-1] + "_scale"]
                 elif key + "_scale" in values:
-                    scale = meter_values[key + "_scale"]
+                    scale = values[key + "_scale"]
 
                 if scale < 0:
                     value = value / 10**abs(scale)
 
-                meter_data["fields"].update({key: value})
+                inverter_data["fields"].update({key: value})
 
-        json_body.append(meter_data)
+        json_body.append(inverter_data)
 
-    # read batteries
-    for battery,params in batteries.items():
-        battery_values = params.read_all()
+        # read meters
+        for meter,params in meters.items():
+            meter_values = params.read_all()
+            meter_data = {
+                "measurement": "meter",
+                "tags": {
+                    "c_model": meter_values["c_model"],
+                    "c_option": meter_values["c_option"],
+                    "c_version": meter_values["c_version"],
+                    "c_serialnumber": meter_values["c_serialnumber"],
+                    "c_deviceaddress": meter_values["c_deviceaddress"],
+                    "c_sunspec_did": meter_values["c_sunspec_did"]
+                },
+                "time": current_time,
+                "fields": {}
+            }
 
-        battery_data = {
-            "measurement": "battery",
-            "tags": {
-                "manufacturer_name": battery_values["manufacturer_name"],
-                "model": battery_values["model"],
-                "firmware_version": battery_values["firmware_version"],
-                "serial_number": battery_values["serial_number"],
-                "device_id": battery_values["device_id"],
-            },
-            "time": current_time,
-            "fields": {}
-        }
+            for key,value in meter_values.items():
+                if (isinstance(value, int) or isinstance(value, float)) and '_scale' not in key:
+                    # search scale
+                    splitted_key = key.split("_")
+                    scale = 0
+                    if splitted_key[len(splitted_key)-1] + "_scale" in values:
+                        scale = meter_values[splitted_key[len(splitted_key)-1] + "_scale"]
+                    elif key + "_scale" in values:
+                        scale = meter_values[key + "_scale"]
 
-        for key,value in battery_values.items():
-            if isinstance(value, int) or isinstance(value, float):
-                battery_data["fields"].update({key: value})
+                    if scale < 0:
+                        value = value / 10**abs(scale)
 
-        json_body.append(battery_data)
+                    meter_data["fields"].update({key: value})
 
-    #print(json.dumps(json_body, indent=4))
+            json_body.append(meter_data)
 
-    client.write_points(json_body)
+        # read batteries
+        for battery,params in batteries.items():
+            battery_values = params.read_all()
+
+            battery_data = {
+                "measurement": "battery",
+                "tags": {
+                    "manufacturer_name": battery_values["manufacturer_name"],
+                    "model": battery_values["model"],
+                    "firmware_version": battery_values["firmware_version"],
+                    "serial_number": battery_values["serial_number"],
+                    "device_id": battery_values["device_id"],
+                },
+                "time": current_time,
+                "fields": {}
+            }
+
+            for key,value in battery_values.items():
+                if isinstance(value, int) or isinstance(value, float):
+                    battery_data["fields"].update({key: value})
+
+            json_body.append(battery_data)
+
+        #print(json.dumps(json_body, indent=4))
+
+        client.write_points(json_body)
+        time.sleep(1)
