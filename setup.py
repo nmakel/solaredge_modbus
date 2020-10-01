6733d0f7a6bb5d0f59ba7db68dcb1757c58c0c8e
niels <51261230+nmakel@users.noreply.github.com>
2020-10-01 09:35:00 +0200

    bump version to 0.6.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="solaredge_modbus",
-    version="0.5.0",
+    version="0.6.0",
     description="SolarEdge Modbus parser library",
     long_description=long_description,
     long_description_content_type="text/markdown",
