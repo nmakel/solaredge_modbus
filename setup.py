62c590f00d297048d5cc4d7dad8ef23637aa3020
niels <51261230+nmakel@users.noreply.github.com>
2021-06-08 00:12:27 +0200

    migrated to setup.cfg with pyproject.toml

--- setup.py
+++ setup.py
@@ -1,33 +1,3 @@
-#!/usr/bin/env python3
-
 import setuptools
 
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="solaredge_modbus",
-    version="0.6.5",
-    description="SolarEdge Modbus parser library",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="MIT License",
-    author="nmakel",
-    author_email="",
-    url="https://github.com/nmakel/solaredge_modbus",
-    packages=["solaredge_modbus"],
-    include_package_data=True,
-    install_requires=[
-        "pymodbus>=2.4.0"
-    ],
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Programming Language :: Python :: 3.7",
-        "Operating System :: OS Independent",
-        "Environment :: Console",
-        "Natural Language :: English",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License"
-    ]
-)
+setuptools.setup()
